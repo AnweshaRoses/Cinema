ee05c357bbb14493447fcf11134582c62300559e
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-09 13:57:35 +0800

    Added light and dark mode

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i) => {
 
-  arrow.addEventListener("click", () => {
-   
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-      }px)`;
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
   });
+  ball.classList.toggle("active");
 });
